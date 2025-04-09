de3ad4407b0e2146fffe27ca316c0365e3939f09
Danick Loic Yagoue <dy@alphabytes.de>
2025-04-09 18:39:05 +0200

    defining first layouts and index page

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <ul>
+                <li>
+                    <NuxtLink to="">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="">Articles</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="">Register</NuxtLink>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <slot></slot>
+        </div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div>
+            <h1>This is the footer</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
