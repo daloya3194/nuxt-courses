99165e1781decaa3468b11a77dd57ccf36044d8c
Danick Loic Yagoue <dy@alphabytes.de>
2025-04-09 19:18:58 +0200

    implementing simple routing with basics layouts

---
+++ pages/blog/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>
+            This is the article {{ id ?? 'Test' }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
